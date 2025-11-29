3e5776963fc5e0aeae53aacd200dde29e16039a9
Rsarith <130320141+SarithRanathunge@users.noreply.github.com>
2025-11-30 00:45:10 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Create a Scanner to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Print greeting
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java!");
+
+        // Ask for name
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+
+        // Say hello to the user
+        System.out.println("Nice to meet you, " + name + "!");
+
+        scanner.close();
+    }
+}
